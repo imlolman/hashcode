99c3b4e5a9d833a182abf55f22720d17ae1e3dba
Satyam Gupta <satyam.gupta756@gmail.com>
2021-01-13 01:32:56 +0530

    Modified Code

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 
-inputFile = "b_little_bit_of_everything.in"
-outputFile = "b_little_bit_of_everything.out"
+inputFile = "c_many_ingredients.in"
+outputFile = "c_many_ingredients.out"
 
 inp = open(inputFile).read().split('\n')
 
@@ -8,8 +8,17 @@ _, duo, trio, squad = list(map(int,inp[0].strip().split(' ')))
 
 # print(duo, trio, squad)
 
-pizzas = list(map(lambda x: x[1:].strip().split(' '), inp[1:-1]))
+def mapPizzas(line, index):
+    list = line[1:].strip().split(' ')
+    list.sort()
+    return {
+        "ingredientsCount": len(list),
+        "ingredients": list,
+        "index": index
+    }
 
+pizzas = [mapPizzas(line, index) for index, line in enumerate(inp[1:-1])]
+pizzas.sort(key=lambda x: x["ingredientsCount"], reverse=True)
 # print(pizzas)
 
 # {indexes, teamType}
@@ -19,8 +28,8 @@ def packAndSend(pizzasForTeam, teamType):
     indexes = []
     for pizzaForTeam in pizzasForTeam:
         # adding None to ignore the pizza in future
-        pizzas[pizzaForTeam["index"]].append(None)
-        indexes.append(pizzaForTeam["index"])
+        indexes.append(pizzaForTeam["pizza"]["index"])
+        pizzas.remove(pizzaForTeam["pizza"])
 
     submission.append({
         "indexes": indexes,
@@ -28,95 +37,43 @@ def packAndSend(pizzasForTeam, teamType):
     })
     
 
-while squad:
-    teamSize = 4
-    # Unique items for a team
-    unique = []
+def calculateForTeam(teamsCount, teamSize):
+    while teamsCount:
+        # Unique items for a team
+        unique = []
 
-    # {pizzaId, value}
-    pizzasForTeam = []
+        # {pizzas, value}
+        pizzasForTeam = []
 
-    for index,pizza in enumerate(pizzas):
-        value = 0
-        if None in pizza:
-            continue
-        for item in pizza:
-            if item not in unique:
-                value += 1
-                unique.append(item)
-        if value != 0:
-            pizzasForTeam.append({
-                "index": index,
-                "value": value
-            })
-    if len(pizzasForTeam) >= teamSize:
-        pizzasForTeam.sort(key=lambda x: x["value"], reverse=True)
-        packAndSend(pizzasForTeam[0:teamSize], teamSize)
-        squad-=1
-    else:
-        break
+        for pizza in pizzas:
+            value = 0
+            for ingredient in pizza["ingredients"]:
+                if ingredient not in unique:
+                    value += 1
+                    unique.append(ingredient)
+            if value != 0:
+                pizzasForTeam.append({
+                    "pizza": pizza,
+                    "value": value
+                })
+        if len(pizzasForTeam) >= teamSize:
+            pizzasForTeam.sort(key=lambda x: x["value"], reverse=True)
+            packAndSend(pizzasForTeam[0:teamSize], teamSize)
+            teamsCount-=1
+        else:
+            break
+    print("packed for 1 team")
 
-while trio:
-    teamSize = 3
-    # Unique items for a team
-    unique = []
+print("Calculating for Squad")
+calculateForTeam(squad, 4)
+print("Calculating for trio")
+calculateForTeam(trio, 3)
+print("Calculating for duo")
+calculateForTeam(duo, 2)
 
-    # {pizzaId, value}
-    pizzasForTeam = []
-
-    for index,pizza in enumerate(pizzas):
-        value = 0
-        if None in pizza:
-            continue
-        for item in pizza:
-            if item not in unique:
-                value += 1
-                unique.append(item)
-        if value != 0:
-            pizzasForTeam.append({
-                "index": index,
-                "value": value
-            })
-    if len(pizzasForTeam) >= teamSize:
-        pizzasForTeam.sort(key=lambda x: x["value"], reverse=True)
-        trio-=1
-        packAndSend(pizzasForTeam[0:teamSize], teamSize)
-    else:
-        break
-    
-
-while duo:
-    teamSize = 2
-    # Unique items for a team
-    unique = []
-
-    # {pizzaId, value}
-    pizzasForTeam = []
-
-    for index,pizza in enumerate(pizzas):
-        value = 0
-        if None in pizza:
-            continue
-        for item in pizza:
-            if item not in unique:
-                value += 1
-                unique.append(item)
-        if value != 0:
-            pizzasForTeam.append({
-                "index": index,
-                "value": value
-            })
-    if len(pizzasForTeam) >= teamSize:
-        pizzasForTeam.sort(key=lambda x: x["value"], reverse=True)
-        duo-=1
-        packAndSend(pizzasForTeam[0:teamSize], teamSize)
-    else:
-        break
 
 def mapSubmission(sub):
     return ' '.join([str(sub["teamType"]), *list(map(str, sub["indexes"]))])
-
-    
 
 open(outputFile, "w+").write('\n'.join([str(len(submission)), *list(map(mapSubmission, submission))]))
 
